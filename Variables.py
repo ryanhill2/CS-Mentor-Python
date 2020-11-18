19e658ed8b4a5aa97bac2585efd77b5881875ef2
ryan <ryan.hill1@mydit.ie>
2020-11-18 22:24:12 +0000

    working with variables and data types

---
+++ Variables.py
@@ -0,0 +1,24 @@
+num = 5
+float1 = 15.5
+string1 = "Ryan"
+string2 = 'Kris'
+char = "R"
+char = 'K'
+my = False
+
+# 1var = 6
+# my var = 12
+# print(num1+num2)
+# myBool = fase
+
+print(10 + 5.8)
+# print(10+B)
+# print(10 + 'B')
+name = 'ryan'
+print("B" + name)
+
+age = 25
+print(age - 18)
+
+
+
